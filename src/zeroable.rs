dabe456f8f578705ae25c5d0f0ae9267315de763
Michelle Granat <michelle.gra.frl@gmail.com>
2024-11-30 09:56:38 +0200

    Added tests for Zeroable trait implementations

--- src/zeroable.rs
+++ src/zeroable.rs
@@ -99,3 +99,97 @@ impl<T: Zeroable + Copy, const N: usize> Zeroable for [T; N] {
         [T::zeroed(); N]
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn zeroed_bool() {
+        assert_eq!(bool::zeroed(), false);
+    }
+
+    #[test]
+    fn zeroed_u8() {
+        assert_eq!(u8::zeroed(), 0u8);
+    }
+
+    #[test]
+    fn zeroed_u16() {
+        assert_eq!(u16::zeroed(), 0u16);
+    }
+
+    #[test]
+    fn zeroed_u32() {
+        assert_eq!(u32::zeroed(), 0u32);
+    }
+
+    #[test]
+    fn zeroed_u64() {
+        assert_eq!(u64::zeroed(), 0u64);
+    }
+
+    #[test]
+    fn zeroed_u128() {
+        assert_eq!(u128::zeroed(), 0u128);
+    }
+
+    #[test]
+    fn zeroed_usize() {
+        assert_eq!(usize::zeroed(), 0usize);
+    }
+
+    #[test]
+    fn zeroed_i8() {
+        assert_eq!(i8::zeroed(), 0i8);
+    }
+
+    #[test]
+    fn zeroed_i16() {
+        assert_eq!(i16::zeroed(), 0i16);
+    }
+
+    #[test]
+    fn zeroed_i32() {
+        assert_eq!(i32::zeroed(), 0i32);
+    }
+
+    #[test]
+    fn zeroed_i64() {
+        assert_eq!(i64::zeroed(), 0i64);
+    }
+
+    #[test]
+    fn zeroed_i128() {
+        assert_eq!(i128::zeroed(), 0i128);
+    }
+
+    #[test]
+    fn zeroed_isize() {
+        assert_eq!(isize::zeroed(), 0isize);
+    }
+
+    #[test]
+    fn zeroed_f32() {
+        assert_eq!(f32::zeroed(), 0f32);
+    }
+
+    #[test]
+    fn zeroed_f64() {
+        assert_eq!(f64::zeroed(), 0f64);
+    }
+
+    #[test]
+    fn zeroed_arrays() {
+        assert_eq!(<[bool; 3]>::zeroed(), [false, false, false]);
+        assert_eq!(<[u8; 1]>::zeroed(), [0u8]);
+        assert_eq!(<[i8; 5]>::zeroed(), [0i8, 0i8, 0i8, 0i8, 0i8]);
+        assert_eq!(<[f32; 2]>::zeroed(), [0f32, 0f32]);
+        assert_eq!(<[f64; 10]>::zeroed(), [
+            0f64, 0f64, 0f64,
+            0f64, 0f64, 0f64,
+            0f64, 0f64, 0f64,
+            0f64
+        ]);
+    }
+}
