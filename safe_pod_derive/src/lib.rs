c772ab5e14533412b36ec3b6f0ea147950d6e1f8
Michelle Granat <michelle.gra.frl@gmail.com>
2025-01-02 13:46:28 +0200

    Updated macro docs

--- safe_pod_derive/src/lib.rs
+++ safe_pod_derive/src/lib.rs
@@ -10,6 +10,8 @@ use syn::{parse_macro_input, DeriveInput};
 /// Derive macro for the Zeroable trait
 /// 
 /// # Example
+/// In order to derive the `Zeroable` trait for a struct,
+/// all of its' fields must also implement `Zeroable`.
 /// ```
 /// #[derive(Debug, Zeroable)]
 /// struct Foo {
@@ -22,6 +24,28 @@ use syn::{parse_macro_input, DeriveInput};
 /// 
 /// println!("Foo zeroed: {:#?}", zeroed_foo);
 /// ```
+/// 
+/// <br/>
+/// 
+/// In order to derive the `Zeroable` trait for an enum,
+/// all of its' variants fields must also implement `Zeroable`.
+/// The "zero variant" must be marked with `#[zero]`.
+/// ```
+/// #[derive(Debug, Zeroable)]
+/// enum MyEnum {
+///     UnitVariant,
+///     TupleVariant(u8, f32),
+///     #[zero]
+///     StructVariant {
+///         x: u32,
+///         y: u32,
+///     },
+/// }
+/// 
+/// let zeroed_my_enum = MyEnum::zeroed();
+/// 
+/// println!("MyEnum zeroed: {:#?}", zeroed_my_enum);
+/// ```
 #[proc_macro_derive(Zeroable, attributes(zero))]
 pub fn derive_zeroable(input: TokenStream) -> TokenStream {
     zeroable::derive_zeroable_impl(
@@ -32,6 +56,8 @@ pub fn derive_zeroable(input: TokenStream) -> TokenStream {
 /// Derive macro for the Pod trait
 /// 
 /// # Example
+/// In order to derive the `Pod` trait for a struct,
+/// all of its' fields must also implement `Pod`.
 /// ```
 /// #[derive(Debug, Pod)]
 /// struct Foo {
@@ -46,6 +72,32 @@ pub fn derive_zeroable(input: TokenStream) -> TokenStream {
 /// 
 /// println!("Foo from bytes: {:#?}", foo_from_bytes);
 /// println!("Foo wrote {} bytes to byte buffer: {:#?}", bytes_written, bytes_from_foo);
+/// ```
+/// 
+/// <br />
+/// 
+/// In order to derive the `Pod` trait for an enum,
+/// it must be a [unit-like enum](https://doc.rust-lang.org/reference/items/enumerations.html#unit-only-enum).
+/// The enum must have the `#[pod(...)]` atrribute with the inner attribute `repr($type)`
+/// set to a type that implements `Pod`. Every variant must also have the `#[pod(...)]` 
+/// atrribute with the inner attribute `match_expr($expression)` set to an expression of the
+/// type set in `repr($tpye)`.
+/// ```
+/// #[derive(Debug, Pod)]
+/// #[pod(repr(u32))]
+/// enum UnitLikeEnum {
+///     #[pod(match_expr(0))]
+///     Foo,
+///     #[pod(match_expr(1))]
+///     Bar,
+/// }
+/// 
+/// let enum_from_bytes = UnitLikeEnum::from_le_bytes(&[1, 0, 0, 0])?;
+/// let mut bytes_from_enum = [0u8; UnitLikeEnum::SIZE];
+/// let bytes_written = enum_from_bytes.to_be_bytes(&mut bytes_from_enum)?;
+/// 
+/// println!("UnitLikeEnum from bytes: {:#?}", enum_from_bytes);
+/// println!("UnitLikeEnum wrote {} bytes to byte buffer: {:#?}", bytes_written, bytes_from_enum);
 /// ```
 #[proc_macro_derive(Pod, attributes(pod))]
 pub fn derive_pod(input: TokenStream) -> TokenStream {
